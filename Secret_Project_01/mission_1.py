bbb806d298c9a4ca288b4c8ac35381dd1340bd7a
ZenGorl <lilydeet7@gmail.com>
2025-05-15 20:04:55 +0000

    Add Extention

---
+++ Secret_Project_01/mission_1.py
@@ -0,0 +1,13 @@
+# GIVEAWAY PICKER
+# Choose winners from list
+
+names = ["Alice", "Bob", "Charlie", "Dina", "Eve", "Frank", "Grace", "Heidi", "Ivan", "Judy", 
+         "Karl", "Laura", "Mallory", "Nick", "Olivia", "Peggy", "Quentin", "Rupert", "Sally", "Tom", "Zen"]
+
+def winners(lst):
+    print(lst[0])
+    print(lst[-1])
+    print(lst[9])
+    print(lst[19])
+
+winners(names)
