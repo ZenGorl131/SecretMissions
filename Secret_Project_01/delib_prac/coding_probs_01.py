db2f1c5f9d9c99e4819855e21f16f074e5b7cf3c
ZenGorl <lilydeet7@gmail.com>
2025-05-15 20:38:04 +0000

    Add Problems

---
+++ Secret_Project_01/delib_prac/coding_probs_01.py
@@ -0,0 +1,16 @@
+names = [
+    "Aiden", "Bella", "Caleb", "Daphne", "Ethan", "Fiona", "Gavin", "Hazel", "Ian", "Jasmine",
+    "Kai", "Luna", "Milo", "Nora", "Owen", "Piper", "Quinn", "Riley", "Sienna", "Theo",
+    "Uma", "Violet", "Wyatt", "Xander", "Yara", "Zane", "Aria", "Blake", "Cora", "Declan",
+    "Elena", "Felix", "Gianna", "Holden", "Isla", "Jude", "Kira", "Leo", "Maya", "Noah",
+    "Olive", "Paxton", "Quincy", "Rowan", "Sage", "Tessa", "Uriel", "Valerie", "Wren", "Zen"
+]
+
+# Define a function that takes a list argument and returns a variable that holds the first and last elements
+
+
+# Define a function that removes the last element of a list 
+
+
+# Define a function that inserts argument 2 into the beginning of a list
+    # HINT this will take two arguments
